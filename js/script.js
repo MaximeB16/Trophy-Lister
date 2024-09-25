0fa46eeb2d1d3c961f1ee365cb1ecc50c45aa0b0
MaximeB16 <127118309+MaximeB16@users.noreply.github.com>
2024-09-25 14:42:17 +0200

    Add files via upload
    Version 1.0 - Fonctionnalitées : - Menu
                                                            - Barre de navigation
                                                            - Formulaire
    Non Fonctionnel

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function openNav() {document.getElementById("mySidenav").classList.add("active");}
+function closeNav() {document.getElementById("mySidenav").classList.remove("active");}
+function openin() {open("article.html")}
+function opengame() {open("trophes.html")}
